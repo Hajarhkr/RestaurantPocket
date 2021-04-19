84331c6def14ca036f97c66dde072d6233eeef57
Hajar HKR <elhakourhajar@gmail.com>
2021-04-19 22:55:21 +0000

    Linked with Backend

--- src/Components/Plat.js
+++ src/Components/Plat.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
-import { Card, Form, Button, Col } from 'react-bootstrap'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faSave, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import { Card, Form, Button, Col, FormGroup } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 
 const CardColor = { backgroundColor: '#f7f6e7' };
 
 export default class Plat extends Component {
-    constructor (props){
+    constructor(props) {
         super(props);
-        this.state={nomplat:'', description:'', prix:'', categorie:'' }; 
-        this.platChange= this.platChange.bind(this);
-        this.submitPlat=this.submitPlat.bind(this);
+        this.state = { nomplat: '', description: '', prix: '', categorie: '', coverPhotoURL:''};
+        this.platChange = this.platChange.bind(this);
+        this.submitPlat = this.submitPlat.bind(this);
     }
 
-    submitPlat(event){
-        alert('Nom:'+this.state.nomplat + ' description:'+this.state.description+' Prix:'+this.state.prix+ ' Categorie:'+this.state.categorie);
+    submitPlat(event) {
+        alert('Nom:' + this.state.nomplat + ' description:' + this.state.description + ' Prix:' + this.state.prix + ' Categorie:' + this.state.categorie + ' Photo:' + this.state.coverPhotoURL);
         event.preventDefault();
 
     }
 
-    platChange (event) {
-        
+    platChange(event) {
+
         this.setState({
-            [event.target.name]:event.target.value
+            [event.target.name]: event.target.value
         });
     };
 
@@ -40,43 +40,64 @@ export default class Plat extends Component {
                             <Form.Control required
                                 type="text" name="nomplat"
                                 value={this.state.nomplat}
-                                onChange={this.platChange} 
+                                onChange={this.platChange}
                                 className="bg-light text-dark"
                                 placeholder="Entrer le nom du plat" />
                         </Form.Group>
-                        <Form.Group as={Col}  controlId="formGridDescription">
+                        <Form.Group as={Col} controlId="formGridDescription">
                             <Form.Label>Description:</Form.Label>
                             <Form.Control required
                                 type="text" name="description"
                                 value={this.state.description}
-                                onChange={this.platChange} 
+                                onChange={this.platChange}
                                 className="bg-light text-dark"
                                 placeholder="Decrivez brièvement votre plat" />
                         </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                        <Form.Group as={Col}  controlId="formGridPrix">
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridPrix">
                             <Form.Label>Prix:</Form.Label>
                             <Form.Control required
                                 type="number" name="prix"
                                 value={this.state.prix}
-                                onChange={this.platChange} 
+                                onChange={this.platChange}
                                 className="bg-light text-dark"
                                 placeholder="Le prix en DH" />
                         </Form.Group>
-                        <Form.Group as={Col}  controlId="formGridCategorie">
+                        <Form.Group as={Col} controlId="formGridCategorie">
                             <Form.Label>Catégorie:</Form.Label>
                             <Form.Control required
                                 type="text" name="categorie"
                                 value={this.state.categorie}
-                                onChange={this.platChange} 
+                                onChange={this.platChange}
                                 className="bg-light text-dark"
                                 placeholder="catégorie" />
                         </Form.Group>
+                        {/* <FormGroup as={Col}>
+                            <Form.Label>Catégorie:</Form.Label>
+                            <Form.Control as="select" placeholder="Choisir une catégorie">
+                                <option>Plat principal</option>
+                                <option>Dessert</option>
+                            </Form.Control>
+                        </FormGroup> */}
+                        
+                    </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Label>Cover Photo URL</Form.Label>
+                            
+                            <Form.Control 
+                                type="text" name="coverPhotoURL"
+                                value={this.state.coverPhotoURL}
+                                onChange={this.platChange}
+                                className={"bg-light text-dark"}
+                                placeholder="Enter Book Cover Photo URL" />
+                            
+                        </Form.Group>
                     </Form.Row>
                 </Card.Body>
-                <Card.Footer style={{"textAlign":"right"}}>
-                    <Button 
+                <Card.Footer style={{ "textAlign": "right" }}>
+                    <Button
                         size="sm"
                         variant="success"
                         type="submit">
--- src/Components/PlatList.js
+++ src/Components/PlatList.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
-import { Card, Table } from 'react-bootstrap'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faList } from '@fortawesome/free-solid-svg-icons'
+import { Button, ButtonGroup, Card, Table, Image } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
-const TableColor = {backgroundColor:'#FFFFFF'};
-const CardColor = {backgroundColor:'#f7f6e7'};
+const TableColor = { backgroundColor: '#FFFFFF' };
+const CardColor = { backgroundColor: '#f7f6e7' };
 
 export default class PlatList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            plats: []
+        }
+    }
+
+    componentDidMount() {
+        this.findAllPlats();
+    }
+
+    findAllPlats() {
+        axios.get("http://localhost:8080/api/plats")
+            .then(response => response.data)
+            .then((data) =>
+                this.setState({ plats: data }));
+    }
+
+
+
     render() {
         return (
-            <Card  style={CardColor}>
+            <Card style={CardColor}>
                 <Card.Header><FontAwesomeIcon icon={faList} />  Liste de Plats</Card.Header>
                 <Card.Body>
                     <Table border hover striped variant="light" style={TableColor}>
@@ -19,13 +41,33 @@ export default class PlatList extends Component {
                                 <th>Catégorie</th>
                                 <th>Description</th>
                                 <th>Prix</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr align="center">
-                                <td colSpan="4"> Pas de plat enregistré</td>
-                               
-                            </tr>
+                            {this.state.plats.length === 0 ?
+                                <tr align="center">
+                                    <td colSpan="5"> {this.state.plats.length} plats enregistrés</td>
+
+                                </tr> :
+                                this.state.plats.map((plat) => (
+                                    <tr key={plat.id}>
+                                        <td>
+                                            <Image src={plat.coverPhotoURL} rounded width="25" height="25" />
+                                            {plat.nomplat}
+                                        </td>
+                                        <td>{plat.categorie}</td>
+                                        <td>{plat.description}</td>
+                                        <td>{plat.prix} DH</td>
+                                        <td>
+                                            <ButtonGroup>
+                                                <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>
+                                                <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                                            </ButtonGroup>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
 
                     </Table>
